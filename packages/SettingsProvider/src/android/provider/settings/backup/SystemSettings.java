1bc36606b79be711cfb68fad8b1de6a28576cd86
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 09:47:15 +0530

    base: Allow disabling ripple effect on unlock [1/2]
    neobuddy89:
    * Reverse logic. Enabled animation by default.
    
    ArrowOS:
    * Add keys into backup and validators.
    
    Change-Id: Idecaa59547159d0a2b0ecdb55a207377bd466710
    Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -95,5 +95,6 @@ public class SystemSettings {
         Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
         Settings.System.NOTIFICATION_VIBRATION_PATTERN,
         Settings.System.CUSTOM_NOTIFICATION_VIBRATION_PATTERN,
+        Settings.System.ENABLE_RIPPLE_EFFECT,
     };
 }
