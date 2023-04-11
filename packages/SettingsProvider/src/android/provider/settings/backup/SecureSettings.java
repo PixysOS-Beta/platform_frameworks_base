5bc209ce09b814d915666329a6393af435c2fcca
ezio84 <brabus84@gmail.com>
2023-04-11 20:20:59 +0300

    base: Allow showing Ambient instead of fully waking [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    Ido Ben-Hur (YAAP):
    Separate for dt2w and lift to wake
    Better code
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: I4a0db9bd89abecab4ec8fce45795a37095f4c638
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -246,5 +246,7 @@ public class SecureSettings {
         Settings.Secure.SEARCH_PRESS_HOLD_NAV_HANDLE_ENABLED,
         Settings.Secure.SEARCH_LONG_PRESS_HOME_ENABLED,
         Settings.Secure.SYSTEM_BLACK_THEME,
+        Settings.Secure.DOZE_DOUBLE_TAP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT
     };
 }
