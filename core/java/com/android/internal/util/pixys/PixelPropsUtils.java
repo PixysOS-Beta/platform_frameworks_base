7b65cd0553ef01eb687de6247452bc229e7167b3
aswin7469 <aswinas@pixysos.com>
2023-06-15 18:07:12 +0530

    PixelPropUtils: Spoof emoji wallpaper
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/pixys/PixelPropsUtils.java
+++ core/java/com/android/internal/util/pixys/PixelPropsUtils.java
@@ -65,7 +65,8 @@ public class PixelPropsUtils {
             "com.google.android.apps.privacy.wildlife",
             "com.google.android.apps.wallpaper.pixel",
             "com.google.android.apps.wallpaper",
-            "com.google.android.apps.subscriptions.red"
+            "com.google.android.apps.subscriptions.red",
+            "com.google.android.apps.emojiwallpaper"
     };
 
    private static final String[] packagesToChangePixel6Pro = {
