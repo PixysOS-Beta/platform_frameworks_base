dacc218870c7a8e51f48080c1443db069ee9ea48
aswin7469 <aswinas@pixysos.com>
2023-06-16 00:02:16 +0530

    gg

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -869,6 +869,10 @@ public class ApplicationPackageManager extends PackageManager {
             "com.google.android.apps.photos.nexus_preload"
     };
 
+    private static final String[] featuresAndroid = {
+            "android.software.freeform_window_management"
+    };
+
     @Override
     public boolean hasSystemFeature(String name, int version) {
         String packageName = ActivityThread.currentPackageName();
