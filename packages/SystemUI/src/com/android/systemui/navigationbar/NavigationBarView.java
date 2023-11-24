26e1bd81a450e2f69e33b5d6329b1d29ad340365
John Galt <johngaltfirstrun@gmail.com>
2023-11-24 10:46:58 -0500

    Revert "SystemUI: Nuke oriented navbar handle"
    This reverts commit ac789d37964057548c9d8f32eeca7fd854b0a9b8.
    
    Breaks back gesture in fullscreen videos.

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -1169,6 +1169,8 @@ public class NavigationBarView extends FrameLayout {
 
         pw.println("    mScreenOn: " + mScreenOn);
 
+
+        dumpButton(pw, "back", getBackButton());
         dumpButton(pw, "home", getHomeButton());
         dumpButton(pw, "handle", getHomeHandle());
         dumpButton(pw, "rcnt", getRecentsButton());
