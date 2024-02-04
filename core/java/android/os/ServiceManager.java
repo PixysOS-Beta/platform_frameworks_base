14fa43b918001f3af499a2885015cb23de525f8e
aswin7469 <aswinas@pixysos.com>
2024-02-04 20:48:23 +0530

    Revert "ServiceManager: Handle all types of exceptions"
    This reverts commit 274077fb40890d12f9e9debbcfe6a9781776527c.

--- core/java/android/os/ServiceManager.java
+++ core/java/android/os/ServiceManager.java
@@ -245,7 +245,7 @@ public final class ServiceManager {
     public static boolean isDeclared(@NonNull String name) {
         try {
             return getIServiceManager().isDeclared(name);
-        } catch (Exception e) {
+        } catch (RemoteException e) {
             Log.e(TAG, "error in isDeclared", e);
             return false;
         }
