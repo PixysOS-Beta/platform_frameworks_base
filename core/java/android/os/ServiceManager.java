cf90545ca3706b774edf49541f9f4f4a946f9c12
aswin7469 <aswinas@pixysos.com>
2024-02-04 22:55:04 +0530

    tmp

--- core/java/android/os/ServiceManager.java
+++ core/java/android/os/ServiceManager.java
@@ -245,6 +245,9 @@ public final class ServiceManager {
     public static boolean isDeclared(@NonNull String name) {
         try {
             return getIServiceManager().isDeclared(name);
+	} catch (SecurityException se) {
+            Log.e(TAG, "SecurityException in isDeclared", se);
+            return false;
         } catch (RemoteException e) {
             Log.e(TAG, "error in isDeclared", e);
             return false;
