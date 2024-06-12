4f6667b5f88219ae14360566db1baf06e9ddf964
aswin7469 <aswinas@pixysos.com>
2024-06-12 14:13:47 +0530

    double tap to sleep gesture fuckery
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationShadeWindowViewController.java
@@ -361,18 +361,15 @@ public class NotificationShadeWindowViewController implements Dumpable {
                 }
 
                 mFalsingCollector.onTouchEvent(ev);
-<<<<<<< HEAD
-                mPulsingWakeupGestureHandler.onTouchEvent(ev);
 
                 if (mGlanceableHubContainerController.onTouchEvent(ev)) {
                     return logDownDispatch(ev, "dispatched to glanceable hub container", true);
-=======
+               }
                 mQQSGestureHandler.onTouchEvent(ev);
                 // Pass touch events to the pulsing gesture listener only if it's dozing,
                 // otherwise lockscreen DT2S and AOD DT2W will conflict.
                 if (mStatusBarStateController.isDozing()) {
-                    mPulsingWakeupGestureHandler.onTouchEvent(ev);
->>>>>>> 9da518343f00 (SystemUI: Add double tap to sleep gesture)
+                mPulsingWakeupGestureHandler.onTouchEvent(ev);
                 }
                 if (mDreamingWakeupGestureHandler != null
                         && mDreamingWakeupGestureHandler.onTouchEvent(ev)) {
